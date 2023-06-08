840dc5b5bced8b92c23ae8b568ad14294968d412
Ferran <ferranramirezmarti@gmail.com>
2023-06-08 23:27:32 +0200

    no base
    config
    
    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ferranJS.github.io',
-  base: '/',
+  base: '/astro-landing-page',
   integrations: [tailwind()],
 });
