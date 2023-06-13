c21ddda6977ed88478880de6355a21ef5b96b205
Ferran <ferranramirezmarti@gmail.com>
2023-06-13 22:06:17 +0200

    i18n utils and languages

---
+++ src/i18n/languages.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.tasle': 'Tasle',
+  },
+  es: {
+    'nav.home': 'Acceuil',
+    'nav.about': 'À propos',
+  },
+} as const;
