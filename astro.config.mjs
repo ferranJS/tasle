2a0d70a175680e05b8d8173187928504775f74ba
Ferran <ferranramirezmarti@gmail.com>
2023-07-03 15:42:24 +0200

    using vite's import.meta.env.BASE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
-export const base = "/astro-tailwind-landing-0"
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://ferranJS.github.io",
-  base,
+  base: "/astro-tailwind-landing-0",
   integrations: [tailwind()],
 })
