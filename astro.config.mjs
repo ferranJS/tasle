535c8d6e1f17d6f3906a3e8df8080e5e6282a225
Ferran <ferranramirezmarti@gmail.com>
2023-07-03 15:28:27 +0200

    exporting base const from config instead of AstroConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
+export const base = "/astro-tailwind-landing-0"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ferranJS.github.io",
-  base: "/astro-tailwind-landing-0",
+  base,
   integrations: [tailwind()],
 })
